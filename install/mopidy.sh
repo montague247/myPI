a187be8aa142880d376507fe4e392af93b67a9b2
montague247 <woop@Dirks-MacBook-Pro.local>
2016-03-14 00:19:34 +0100

    Copy default config

--- install/mopidy.sh
+++ install/mopidy.sh
@@ -2,6 +2,7 @@
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
 sudo apt-get install mopidy rsync python-pip python-dev build-essential --yes
+sudo cp etc/mopidy/mopidy.conf /etc/mopidy/mopidy.conf
 sudo pip install Mopidy-Local-SQLite
 sudo pip install Mopidy-Local-Images
 sudo pip install Mopidy-API-Explorer
