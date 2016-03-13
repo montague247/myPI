9909d9d96bf230dd7b86cdb01673e2b6753e0f94
montague247 <woop@Dirks-MacBook-Pro.local>
2016-03-13 22:42:42 +0100

    Scripts for installing shairport

---
+++ install/shairport.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -e
+.././update.sh
+sudo apt-get install avahi-utils libssl-dev libao-dev libpulse-dev libasound2-dev
+git clone https://github.com/abrasive/shairport.git ../shairport
+cd ../shairport
+./configure
+make
+sudo make install
+sudo cp -r scripts/debian/* /etc/
+sudo adduser --system --disabled-login --ingroup audio shairport
+cd myPI
+sudo cp config/shairport /etc/default/shairport
