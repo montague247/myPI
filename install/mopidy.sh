fa0e0eccbb343fde58967f669f7164a1fe44c6d6
montague247 <woop@Dirks-MacBook-Pro.local>
2016-03-13 23:39:43 +0100

    Added sudo

--- install/mopidy.sh
+++ install/mopidy.sh
@@ -2,16 +2,16 @@
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
 sudo apt-get install mopidy rsync python-pip python-dev build-essential --yes
-pip install Mopidy-Local-SQLite
-pip install Mopidy-Local-Images
-pip install Mopidy-API-Explorer
-pip install Mopidy-Material-Webclient
-pip install Mopidy-Mobile
-pip install Mopidy-Moped
-pip install Mopidy-Mopify
-pip install Mopidy-MusicBox-Webclient
-pip install Mopidy-Party
-pip install Mopidy-Simple-Webclient
-pip install Mopidy-Spotmop
-pip install Mopidy-WebSettings
-pip install Mopidy-ALSAMixer
+sudo pip install Mopidy-Local-SQLite
+sudo pip install Mopidy-Local-Images
+sudo pip install Mopidy-API-Explorer
+sudo pip install Mopidy-Material-Webclient
+sudo pip install Mopidy-Mobile
+sudo pip install Mopidy-Moped
+sudo pip install Mopidy-Mopify
+sudo pip install Mopidy-MusicBox-Webclient
+sudo pip install Mopidy-Party
+sudo pip install Mopidy-Simple-Webclient
+sudo pip install Mopidy-Spotmop
+sudo pip install Mopidy-WebSettings
+sudo pip install Mopidy-ALSAMixer
