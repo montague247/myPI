844663820541ad4beaf0946c530da7abb51ef16d
montague247 <woop@Dirks-MacBook-Pro.local>
2016-03-15 21:29:33 +0100

    Some fixing

--- install/jasper.sh
+++ install/jasper.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -e
 sudo apt-get install vim git-core python-dev python-pip bison libasound2-dev libportaudio-dev python-pyaudio --yes
+git clone https://github.com/jasperproject/jasper-client.git jasper
+sudo pip install --upgrade setuptools
+sudo pip install -r jasper/client/requirements.txt
+chmod +x jasper/jasper.py
 
--- install/shairport.sh
+++ install/shairport.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -e
 sudo apt-get install avahi-utils libssl-dev libao-dev libpulse-dev libasound2-dev
-git clone https://github.com/abrasive/shairport.git ../shairport
-cd ../shairport
+git clone https://github.com/abrasive/shairport.git shairport
+cd shairport
 ./configure
 make
 sudo make install
 sudo cp -r scripts/debian/* /etc/
 sudo adduser --system --disabled-login --ingroup audio shairport
-sudo cp etc/default/shairport /etc/default/shairport
-cd install
+cd ..
+sudo cp ../etc/default/shairport /etc/default/shairport
 sudo update-rc.d shairport defaults
 sudo service shairport start
