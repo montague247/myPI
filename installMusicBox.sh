1a662c6c66bcdf6cf24707424799f44cad85047a
montague247 <woop@Dirks-MacBook-Pro.local>
2016-03-15 18:21:59 +0100

    No questions on install

--- installMusicBox.sh
+++ installMusicBox.sh
@@ -4,6 +4,6 @@ cd install
 #./shairport.sh
 #./mopidy.sh
 cd ..
-sudo apt-get install iptables-persistent
+sudo apt-get install iptables-persistent --yes
 sudo cp etc/firewall/musicbox_iptables /etc/iptables/rules.v4
 sudo cp etc/motd_musicbox /etc/motd
